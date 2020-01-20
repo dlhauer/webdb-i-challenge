6f658e0ba5bf1d1ac485c57ae12db3a18a2bdd0c
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-01-20 13:40:00 -0800

    GET and POST endpoints

--- server.js
+++ server.js
@@ -6,4 +6,58 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  db.select('*').from('accounts')
+    .then(accounts => {
+      // console.log('accounts: ', accounts);
+      res.status(200).json(accounts);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: 'Error retrieving the accounts.'
+      });
+    });
+});
+
+server.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db
+    .select('*')
+    .from('accounts')
+    .where({ id })
+    .first()
+    .then(account => {
+      console.log('account: ', account);
+      res.status(200).json(account);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: 'Error retrieving the account.'
+      });
+    });
+});
+
+server.post('/', (req, res) => {
+  const accountData = req.body;
+  db('accounts')
+    .insert(accountData, 'id')
+    .then(ids => {
+      const [id] = ids;
+      return db('accounts')
+              .select('id', 'name', 'budget')
+              .where({ id })
+              .first()
+              .then(account => {
+                res.status(201).json(account)
+              })
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: 'Error saving new account.'
+      });
+    });
+});
+
 module.exports = server;
